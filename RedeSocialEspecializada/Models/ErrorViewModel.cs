7ffc267bab8080122c3465df7f9567d23c04cd78
Kaio Vasconcelos <Kaiohvs@gmail.com>
2024-06-05 00:39:36 -0300

    Adicionar arquivos de projeto.

---
+++ RedeSocialEspecializada/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RedeSocialEspecializada.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
